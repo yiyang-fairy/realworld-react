42c24a487e7f22baf9dbec0de69ec486b7b21dd6
SedationH <sedationh@gmail.com>
2023-08-23 23:20:48 +0800

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
-import { useEffect, useState } from "react";
-import { router } from "./router/index.jsx";
-import { RouterProvider } from "react-router-dom";
-import "./index.css";
-import { getUser } from "./api/user";
-import { UserContext } from "./utils/user";
-import Header from "./components/Header.jsx";
+import "./index.css"
+import { useState } from "react"
+import { router } from "./router/index.jsx"
+import { RouterProvider } from "react-router-dom"
+import { getUser } from "./api/user"
+import { UserContext } from "./utils/user"
+import { useAsyncEffect } from "ahooks"
 
 function App() {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
+  const [user, setUser] = useState(null)
+  useAsyncEffect(async () => {
     if (localStorage.getItem("token")) {
-      get()
+      const res = await getUser()
+      setUser(res.user)
     }
-  }, []);
-  async function get() {
-    const res = await getUser()
-    setUser(res.user);
-
-  }
-  
-console.log(user, 'app user')
+  }, [])
 
   return (
-    <UserContext.Provider value={{user, setUser}}>
-      <div>
-        <RouterProvider router={router} >
-          <Header></Header>
-          {router}
-        </RouterProvider>
-      </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      <RouterProvider router={router} />
     </UserContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
